b3e396aa581addb8e584541637e6339d3be2688c
Paul Cockrell <paulcockrell@gmail.com>
2023-12-17 11:49:55 +0000

    Initialize app. Add Bevy. Generate window

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Slidey".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .run();
+}
+
